1a7cf33673ad25158187eb68a6109228f7c9c036
Bluu <tailsfang@gmail.com>
2023-10-14 00:24:08 +0200

    fixed drag

--- Physics.py
+++ Physics.py
@@ -1,12 +1,13 @@
 import Geometry as g
 import math
 
-G=6.67*10**-11
+G = 6.67*10**-11
+
 
 class PointMass2():
     def __init__(self, mass, x, y, velocityX, velocityY):
-        self.x = x 
-        self.y = y 
+        self.x = x
+        self.y = y
         self.radius = math.pow(mass*4/3*math.pi, 1/3)
         self.mass = mass
         self.velocityX = velocityX
@@ -15,30 +16,38 @@ class PointMass2():
         self.prevY = y
         self.nextX = x
         self.nextY = y
+
     def getVelocity(self):
-        v = g.distance2(0,0, self.velocityX, self.velocityY)
+        v = g.distance2(0, 0, self.velocityX, self.velocityY)
         return v
+
     def getRadius(self):
         return math.pow(self.mass*4/3*math.pi, 1/3)
-    
+
+
 class PointMass3(g.Point3):
-    #inherits position
-    def __init__(self, mass,x,y,z, velocityX, velocityY, velocityZ):
-        super().__init__(x,y,z)
+    # inherits position
+    def __init__(self, mass, x, y, z, velocityX, velocityY, velocityZ):
+        super().__init__(x, y, z)
         self.mass = mass
         self.velocityX = velocityX
         self.velocityY = velocityY
         self.velocityZ = velocityZ
+
     def velocity(self):
-        v = g.distance3(0,0,0, self.velocityX, self.velocityY, self.velocityZ)
+        v = g.distance3(0, 0, 0, self.velocityX,
+                        self.velocityY, self.velocityZ)
         return v
-    
+
+
 class World2:
-    def __init__(self, lop:list()):
+    def __init__(self, lop: list()):
         self.pointsList = lop
         self.collissions = 0
+
     def totalEnergy(self):
         pass
+
     def update(self, settings):
         ts = settings["timestep"]
         W = settings["width"]
@@ -53,26 +62,28 @@ class World2:
             bounceEnergyLoss = 0.1
             for p in self.pointsList:
                 if p.y-p.getRadius() < 0:
-                    p.y=0+p.getRadius()
-                    #p.velocityY*=-(1-bounceEnergyLoss)
+                    p.y = 0+p.getRadius()
+                    p.velocityY *= -(1)
                 elif p.y+p.getRadius() > H:
-                    p.y=H-p.getRadius()
-                    #p.velocityY*=-(1-bounceEnergyLoss)
+                    p.y = H-p.getRadius()
+                    p.velocityY *= -(1)
                 if p.x-p.getRadius() < 0:
-                    p.x=0+p.getRadius()
-                    #p.velocityX*=-(1-bounceEnergyLoss)
+                    p.x = 0+p.getRadius()
+                    p.velocityX *= -(1)
                 elif p.x+p.getRadius() > W:
-                    p.x=W-p.getRadius()
-                    #p.velocityX*=-(1-bounceEnergyLoss)
+                    p.x = W-p.getRadius()
+                    p.velocityX *= -(1)
 
         if settings["bodyCollissions"]:
             for p in self.pointsList:
                 for other in self.pointsList:
                     if p != other:
                         dist = g.distance2(p.x, p.y, other.x, other.y)
-                        collission = dist[2] < (p.getRadius() + other.getRadius())
+                        collission = dist[2] < (
+                            p.getRadius() + other.getRadius())
                         if collission:
-                            repAcc = -(G*p.mass*other.mass)/(dist[2]) / p.mass #dist[2]**2 potentially
+                            # dist[2]**2 potentially
+                            repAcc = -(G*p.mass*other.mass)/(dist[2]) / p.mass
                             p.velocityX += repAcc*(dist[0]/dist[2])*ts
                             p.velocityY += repAcc*(dist[1]/dist[2])*ts
 
@@ -81,70 +92,66 @@ class World2:
                 for other in self.pointsList:
                     if p != other:
                         dist = g.distance2(p.x, p.y, other.x, other.y)
-                        if dist[2]< p.getRadius():
+                        if dist[2] < p.getRadius():
                             p.mass += other.mass
-                            resvx=p.velocityX + other.velocityX
-                            resvy=p.velocityY + other.velocityY
+                            resvx = p.velocityX + other.velocityX
+                            resvy = p.velocityY + other.velocityY
                             p.velocityX = resvx*(p.mass/(p.mass+other.mass))
                             p.velocityY = resvy*(p.mass/(p.mass+other.mass))
                             self.pointsList.remove(other)
-                            
-                            
-
-
-
-
-
 
         if settings["nbody"]:
             for p in self.pointsList:
                 for other in self.pointsList:
                     if p != other:
-                        newVel=newVelDue2(p, other, ts)
+                        newVel = newVelDue2(p, other, ts)
                         p.velocityX = newVel[0]
                         p.velocityY = newVel[1]
-        
-        
 
-
-        
-        #this runs at the end regardless of which you choose, since al lthe functions only update the velocity
+        # this runs at the end regardless of which you choose, since al lthe functions only update the velocity
         for p in self.pointsList:
-            Cd = 1000
-            p.velocityX = p.velocityX*0.99
-            p.velocityY = p.velocityY*0.99
+            Cd = 10000
+            speedX = abs(p.velocityX)
+            speedY = abs(p.velocityY)
+            dragX = (Cd*speedX**2/2)*g.sign(p.velocityX)
+            dragY = (Cd*speedY**2/2)*g.sign(p.velocityY)
+            p.velocityX -= dragX
+            p.velocityY -= dragY
             p.x += p.velocityX * ts
             p.y += p.velocityY * ts
 
 
-def forceDue2(p:PointMass2, other:PointMass2):
+def forceDue2(p: PointMass2, other: PointMass2):
     dist = g.distance2(p.x, p.y, other.x, other.y)
     resForce = (G * p.mass * other.mass)/dist[2]**2
     return resForce
 
-def accDue2(p:PointMass2, other:PointMass2):
-    f=forceDue2(p, other)
-    dist=g.distance2(p.x, p.y, other.x, other.y)
-    a=f/p.mass
-    ax=a*(dist[0]/dist[2])
-    ay=a*(dist[1]/dist[2])
+
+def accDue2(p: PointMass2, other: PointMass2):
+    f = forceDue2(p, other)
+    dist = g.distance2(p.x, p.y, other.x, other.y)
+    a = f/p.mass
+    ax = a*(dist[0]/dist[2])
+    ay = a*(dist[1]/dist[2])
     return [ax, ay, a]
 
-def newVelDue2(p:PointMass2, other:PointMass2, ts):
+
+def newVelDue2(p: PointMass2, other: PointMass2, ts):
     accXYC = accDue2(p, other)
     velX = accXYC[0]*ts
     velY = accXYC[1]*ts
     return [p.velocityX+velX, p.velocityY+velY]
 
-def dispDue2(p:PointMass2, other:PointMass2, ts):
-    acc=accDue2(p, other)
-    dispX=p.velocityX + 0.5*acc[0]*ts**2
-    dispY=p.velocityY + 0.5*acc[1]*ts**2
+
+def dispDue2(p: PointMass2, other: PointMass2, ts):
+    acc = accDue2(p, other)
+    dispX = p.velocityX + 0.5*acc[0]*ts**2
+    dispY = p.velocityY + 0.5*acc[1]*ts**2
     return [dispX, dispY]
 
-def newPosDue2(p:PointMass2, other:PointMass2, ts):
+
+def newPosDue2(p: PointMass2, other: PointMass2, ts):
     disp = dispDue2(p, other, ts)
     newX = p.x+disp[0]
     newY = p.y+disp[1]
     return [newX, newY]
-
--- main.py
+++ main.py
@@ -3,26 +3,25 @@ from Physics import PointMass2, World2
 import DrawingPygame
 import random
 
-settings={"timestep":1e4, 
-          "width":550, 
-          "height":550, 
-          "nbody":True,
-          "verticalGravity":False,
-          "bodyCollissions":True,
-          "screenCollissions":True,
-          "merging" : True}
+settings = {"timestep": 1e4*2,
+            "width": 550,
+            "height": 550,
+            "nbody": True,
+            "verticalGravity": False,
+            "bodyCollissions": True,
+            "screenCollissions": True,
+            "merging": False}
 
-#generate some particles evenly across the world
+# generate some particles evenly across the world
 li = []
 if True:
-    nw, nh = 6, 6
+    nw, nh = 2, 2
     for n in range(nw):
         for m in range(nh):
             w, h = settings["width"], settings["height"]
-            mass = random.randint(10,50)
-            li.append(PointMass2(mass, w/nw * n + 20, h/nh * m +20, 0, 0))
+            mass = random.randint(10, 50)
+            li.append(PointMass2(mass, w/nw * n + 20, h/nh * m + 20, 0, 0))
 
 
 world = World2(li)
 DrawingPygame.RUNSIM2(world, settings)
-
